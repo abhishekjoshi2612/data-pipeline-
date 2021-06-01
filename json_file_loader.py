a09e451e7f7c1602e26e827f626586b6e9c4bbc4
abhishekjoshi2612 <66815782+abhishekjoshi2612@users.noreply.github.com>
2021-06-01 07:37:48 +0530

    Update json_file_loader.py

--- json_file_loader.py
+++ json_file_loader.py
@@ -3,13 +3,17 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import date, timedelta,datetime
 import os
 import json
-import random
+import 
+#importing a job to run from another file
 from neo_4j import runit
 
+#function to create file in current directory takes name of file and  date supplied
 def file_maker(name,dates):
     final_name = name + "@tribes.ai"
+    #name of each file = name of person + . + current date + .json
     name_of_file = name + "." +str(dates) + ".json"
     cur_sum = 0
+    #assigning values to minute used variable for apps
     google_driver = random.randint(0,180)
     cur_sum = google_driver
     slacker = random.randint(0,min(480 - cur_sum,180))
@@ -22,7 +26,7 @@ def file_maker(name,dates):
     cur_sum = cur_sum + chromer
     spotifyer = random.randint(0,min(480 - cur_sum,180))
 
-  
+    
     my_json_data =  {"user_id" : final_name,
                          "usages_date" : str(dates),
                          "device" : {
@@ -80,7 +84,7 @@ def file_maker(name,dates):
     
 
     
-
+#functions to call when 30 days files not made
 def vinit_30_days(dates):
     file_maker("vinit",dates)
 def guilermo_30_days(dates):
@@ -93,7 +97,7 @@ def eily_30_days(dates):
     
 def abhishek_30_days(dates):
     file_maker("abhishek",dates)
-    
+#functions to call for today files
 def vinit_today(dates):
     file_maker("vinit",dates)
     
@@ -109,12 +113,13 @@ def eily_today(dates):
 def abhishek_today(dates):
     file_maker("abhishek",dates)
     
-
+#this function get called after checker finds that last 30 days file not made
 def create_files():
     delta = timedelta(days=1)
 
     end_date = date.today()
     start_date = end_date - delta*30
+    #using a while loop to create last 30 day files
     while start_date<end_date:
         vinit_30_days(start_date)
         guilermo_30_days(start_date)
@@ -122,6 +127,7 @@ def create_files():
         eily_30_days(start_date)
         abhishek_30_days(start_date)
         start_date += delta
+#this function get called for creating file containing today date        
 def create_today_files():
     dates = date.today()
     print("hello")
@@ -133,17 +139,21 @@ def create_today_files():
 
 def create_last_days():
     curr_path = os.getcwd()
+    #last 30 day files stored in folder named stored_30_day_files
     path = os.path.join(curr_path,"stored_30_days_files")
     os.mkdir(path)
     os.chdir(path)
+    #directory changed to /stored_30_days_files/
     create_files()
+    #now for creating today files directory again changed so that it looks like => today_files stored_30_day_files(folder)
     os.chdir(curr_path)
    
 
 
-
+#this function calls  create_files if 30 days file required    
 def checker():
     if os.path.exists(('stored_30_days_files')) == False:
+    
         create_last_days()
     
     
@@ -158,22 +168,24 @@ with DAG(
             "owner": "airflow",
             "retries": 1,
             "retry_delay": timedelta(minutes=20),
-            "start_date": datetime(2021, 1, 1),
+            "start_date": datetime(2021, 5, 31),
         },
         catchup=False) as f:
-
+    # this job is for creating 30 day files if doesnt exists
     checker = PythonOperator(
         task_id="checker",
         python_callable=checker,
         provide_context=True,
         
     )
+    #this job is for creating today files
     create_today_files = PythonOperator(
         task_id="create_today_files",
         python_callable=create_today_files,
         provide_context=True,
         
     )
+    #this job is for loading data into neo4j database
     runit = PythonOperator(
         task_id = "runit",
         python_callable=runit,
